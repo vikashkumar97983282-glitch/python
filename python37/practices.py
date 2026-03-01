ddff8d6e7bde57510e21307cd23b3fb924406727
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-03-01 21:51:32 +0530

    practice bank system .

---
+++ python37/practices.py
@@ -0,0 +1,55 @@
+data= {
+    "account_no":[123456789101,
+                  123456789102,
+                  123456789103,
+                  123456789104,
+                  123456789105,
+                  123456789106,
+                  123456789107,
+                  123456789108,
+                  123456789109,
+                  123456789110],
+    
+    "name": [ "Vikash kumar sharma",
+            "Ankur kumar mandal",
+            "Ankit kumar sinha",
+            "Ankit kumar sinha",
+            "Ankit kumar sinha",
+            "Satyam mishra",
+            "Vidya sagar yadav",
+            "Ankit kumar sinha",
+            "Prem sagar yadav",
+            "Roshan kumar mandal",],
+    
+    "balance":[100021,
+               100212,
+               121126,
+               521001,
+               587001,
+               424100,
+               875672,
+               572656,
+               628659,
+               528262
+               ]
+
+}
+print(data["account_no"][0])
+print(data["name"][0])
+
+
+
+
+
+
+
+
+"""{123456789101:"Vikash kumar sharma" , "bal":102154},
+            {123456789102:"Ankur kumar mandal" , "bal":215155},
+            {123456789103:"Ankit kumar sinha" , "bal":155116},
+            {123456789104:"Satyam mishra" , "bal":3546496},
+            {123456789105:"Vidya sagar yadav" , "bal":856162},
+            {123456789106:"Ankit kumar sinha"  , "bal":515164},
+            {123456789107:"Prem sagar yadav" , "bal":654616},
+            {123456789108:"Roshan kumar mandal" , "bal":564915}"""
+
